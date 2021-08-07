fdab1671b60a3179babccfb1bd57e05c8da80a41
uggmycar <67260181+uggmycar@users.noreply.github.com>
2021-08-07 10:38:26 +0800

    Update autoload.js

--- assets/autoload.js
+++ assets/autoload.js
@@ -18,40 +18,34 @@ const jq_ui="https://cdn.jsdelivr.net/npm/jquery-ui-dist@1.12.1/jquery-ui.min.js
 
 
 try {
-    $("<link>").attr({href: waifu_css, 	<!---->
 	
-	rel: "stylesheet", type: "text/css"}).appendTo('head');
+    $("<link>").attr({href: waifu_css,rel: "stylesheet", type: "text/css"}).appendTo('head');
+	
     $('body').append(
-	'<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span><span class="fui-chat"></span><span class="fui-eye"></span><span class="fui-user"></span><span class="fui-photo"></span><span class="fui-info-circle"></span><span class="fui-cross"></span></div></div>');
-    $.ajax
-	({url: waifu_css 
-	,dataType:"script", cache: true, success: function() {
+	'<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span><span class="fui-chat"></span><span class="fui-eye"></span><span class="fui-user"></span><span class="fui-photo"></span><span class="fui-cross"></span></div></div>');
+    
+	$.ajax({url: waifu_tips,dataType:"script", cache: true,async: false });
 		 
-        $.ajax({url: live2d_js, 		<!---->
-		
-		dataType:"script", cache: true, success: function() {
+        $.ajax({url: live2d_js,dataType:"script", cache: true, success: function() {
 		 
-        $.ajax({url: jq , 
-		
-		dataType:"script", cache: true, success:function() {
-		 
-        $.ajax({url: jq_ui , 
-		
-		dataType:"script", cache: true, success: function() {
+
             /* 可直接修改部分参数 */
             live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API
             live2d_settings['modelId'] = 5;                  // 默认模型 ID
             live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
             live2d_settings['modelStorage'] = false;         // 不储存模型 ID
 			live2d_settings['aboutPageUrl']         = 'https://www.fghrsh.net/post/123.html';   // 关于首页地址, '{URL 网址}'
+			live2d_settings['canCloseLive2d'] = false;       // 隐藏 关闭看板娘 按钮
+			  live2d_settings['canTurnToHomePage'] = false;    // 隐藏 返回首页 按钮
 			live2d_settings['waifuDraggable']       = 'unlimited';    // 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽), 'unlimited'(自由拖拽)
+			
+			live2d_settings['waifuToolTop'] = '0px';       // 工具栏顶部边距
 			live2d_settings['waifuDraggableRevert'] = true;         // 松开鼠标还原拖拽位置，可选 true(真), false(假)
             /* 在 initModel 前添加 */
             initModel(waifu_json);		<!---->
 			
 			
         }});
-    }});
-	 }});
-	  }});
+   
+	
 } catch(err) { console.log(err+"[Error] JQuery is not defined.") }
