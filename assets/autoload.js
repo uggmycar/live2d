5c56e3ce1386ba632b72c70f90cfc2b66a985291
uggmycar <67260181+uggmycar@users.noreply.github.com>
2021-08-07 12:30:24 +0800

    Update autoload.js

--- assets/autoload.js
+++ assets/autoload.js
@@ -25,18 +25,18 @@ try {
         $.ajax({url: live2d_js,dataType:"script", cache: true,async: false });
 		
             /* 可直接修改部分参数 */
-            live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API
-            live2d_settings['modelId'] = 5;                  // 默认模型 ID
-            live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
-            live2d_settings['modelStorage'] = false;         // 不储存模型 ID
-			live2d_settings['aboutPageUrl'] = 'https://www.fghrsh.net/post/123.html';   // 关于首页地址, '{URL 网址}'
-			live2d_settings['canCloseLive2d'] = true;       // 隐藏 关闭看板娘 按钮
-			live2d_settings['canTurnToHomePage'] = false;    // false隐藏 返回首页 按钮
-			live2d_settings['waifuDraggable']='unlimited';// 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽),'unlimited'(自由拖拽)	
-			live2d_settings['waifuToolTop'] = '0px';       // 工具栏顶部边距
-			live2d_settings['waifuDraggableRevert'] = true; // 松开鼠标还原拖拽位置，可选 true(真), false(假)
+            	live2d_settings['hitokotoAPI'] = "hitokoto.cn";  // 一言 API
+            	live2d_settings['modelId'] = 5;                  // 默认模型 ID
+            	live2d_settings['modelTexturesId'] = 1;          // 默认材质 ID
+            	live2d_settings['modelStorage'] = false;         // 不储存模型 ID
+		live2d_settings['aboutPageUrl'] = 'https://www.fghrsh.net/post/123.html';   // 关于首页地址, '{URL 网址}'
+		live2d_settings['canCloseLive2d'] = true;       // 隐藏 关闭看板娘 按钮
+		live2d_settings['canTurnToHomePage'] = false;    // false隐藏 返回首页 按钮
+		live2d_settings['waifuDraggable']='unlimited';// 拖拽样式，例如 'disable'(禁用), 'axis-x'(只能水平拖拽),'unlimited'(自由拖拽)	
+		live2d_settings['waifuToolTop'] = '0px';       // 工具栏顶部边距
+		live2d_settings['waifuDraggableRevert'] = true; // 松开鼠标还原拖拽位置，可选 true(真), false(假)
             /* 在 initModel 前添加 */
-            initModel(waifu_json);		<!---->
+            	initModel(waifu_json);		<!---->
 		
         
    
